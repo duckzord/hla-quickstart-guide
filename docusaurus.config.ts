276890408f236e2e7be642d2c2dac29800a49f66
Duckzord <tom.gray86@gmail.com>
2025-11-13 17:12:39 +0000

    Add github workflow configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://${organizationName}.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/${projectName}',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'duckzord', // Usually your GitHub org/user name.
+  projectName: 'hla-quickstart-guide', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
